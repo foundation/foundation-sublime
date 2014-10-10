a6f9c4a67189bc84baea3395cd29e607d6d5d31a
Geoff Kimball <geoff@zurb.com>
2014-10-10 13:17:15 -0700

    We've got a sort of working encoder and sort of working decoder

---
+++ decode.py
@@ -0,0 +1,79 @@
+import re
+
+# | = new row
+# , = new column
+
+# sm, md, lg, xl
+# so, mo, lo, xo
+# sp, mp, lp, xp
+
+BREAKPOINTS = {
+  'sg': 'small',
+  'mg': 'medium',
+  'lg': 'large',
+  'xg': 'xlarge',
+  'so': 'small-offset',
+  'mo': 'medium-offset',
+  'lo': 'large-offset',
+  'xo': 'xlarge-offset',
+  'sc': 'small-centered',
+  'mc': 'medium-centered',
+  'lc': 'large-centered',
+  'xc': 'xlarge-centered',
+}
+
+def parse_row(input, depth):
+  row_indent = '\t' * (depth * 2)
+  column_indent = row_indent + '\t'
+
+  # Regular expressions for finding column sizes and column clusters
+  re_size = r"[a-z]{2}-*?[1-9]*(?:\(.*\))?"
+  re_column = r"(?:"+re_size+")+"
+
+  columns = re.findall(re_column, input)
+
+  # Opening tag for row
+  output = '%s<div class="row">\n' % row_indent
+
+  # Processing columns
+  for column in columns:
+    string = column.split('>', 1)
+    base_column = string[0]
+    nested_row  = string[1] if len(string) > 1 else None
+
+    sizes = re.findall(re_size, base_column)
+
+    # Opening tag for column
+    output += '%s<div class="' % column_indent
+
+    # Processing breakpoint classes
+    for size in sizes:
+      bp, number = size[:2], size[2:]
+      output += '%s-%s ' % (BREAKPOINTS[bp], number)
+    output += 'columns">\n'
+
+    # Nested rows
+    if nested_row is not None:
+      output += parse_row(nested_row, depth + 1)
+    else:
+      # Insert a blank line if there's no nested row
+      output += '%s\n' % column_indent
+
+    # Closing tag for column
+    output += '%s</div>\n' % column_indent
+
+  # Closing tag for row
+  output += '%s</div>\n' % row_indent
+  return output
+
+def unencode(input):
+  output = ''
+  rows = input.lower().split('|')
+
+  # Processing rows
+  for row in rows:
+    output += parse_row(row, 0)
+
+  print output
+
+unencode('sg12mg6lo4>')
---
+++ encode.py
@@ -0,0 +1,93 @@
+from HTMLParser import HTMLParser
+import pprint
+
+class foundationParser(HTMLParser):
+  # This is a dictionary that stores the structure of our HTML
+  tree = []
+  # This is an internal counter of what depth the HTML is at
+  depth = -1
+
+  def getTree(self):
+    return self.tree
+
+  def getLastChild(self, l):
+    for i in range(self.depth):
+      l = l[-1]['children']
+    return l
+
+  # This method is run when the parser encounters an opening tag
+  def handle_starttag(self, tag, attrs):
+    attrlist = dict(attrs)
+    # Skip this tag if it doesn't have any grid classes
+    if not any(x in attrlist['class'] for x in ['row', 'column', 'columns']):
+      return
+
+    # Create a new object for this element
+    elem = {
+      'classes': attrlist['class'],
+      'children': [],
+    }
+
+    # We have to go deeper
+    self.depth += 1
+    # If we're at the root of the HTML (depth == 0), append the node to the base object
+    if self.depth is 0:
+      self.tree.append(elem)
+    # Otherwise, find the last node at the current depth and append the node as a child
+    else:
+      self.getLastChild(self.tree).append(elem)
+
+  # This method is run when the parser encounters a closing tag
+  def handle_endtag(self, tag):
+    # Move up one depth level
+    self.depth -= 1
+
+def encodeGrid(tree, root=True):
+  CLASSES = {
+    'small-12': 'sg12',
+    'small-8': 'sg8',
+  }
+
+  output = ''
+  for elem in tree:
+    # Debugging
+    print "Element '%s' has '%s' children." % (elem['classes'], len(elem['children']))
+
+    # If it's a column, get the classes
+    if 'column' in elem['classes']:
+      classes = elem['classes'].split(' ')
+      for cls in classes:
+        if cls in CLASSES:
+          output += '%s,' % CLASSES[cls]
+      # Trim the last hyphen
+      output = output[:-1]
+
+    # We have to go deeper
+    if elem['children']:
+      nestedOutput = encodeGrid(elem['children'], False)
+      # The children of a row are wrapped in parentheses
+      if not root and 'row' in elem['classes']:
+        nestedOutput = '(%s)' % nestedOutput
+      output += nestedOutput
+
+    # If it's a row, stick a comma on the end
+    if 'row' in elem['classes'] and elem is not tree[-1]:
+      output += '|'
+
+  return output
+
+syntax = """
+  <div class="row">
+    <div class="small-8 columns">
+      <div class="row">
+        <div class="small-12 columns"></div>
+        <div class="small-12 columns"></div>
+      </div>
+    </div>
+  </div>
+"""
+
+parser = foundationParser()
+parser.feed(syntax)
+tree = parser.getTree()
+print encodeGrid(tree)
