135972a07d161d7b13394548d0e2c9fa11525178
Geoff Kimball <geoff@zurb.com>
2014-10-13 10:05:32 -0700

    Fix encoder inconsistently handling self-closing tags, and occasionally wigging out when searching the tree for last child

--- encode.py
+++ encode.py
@@ -10,26 +10,34 @@ class foundationParser(HTMLParser):
   # This is an internal counter of what depth the HTML is at
   depth = -1
 
+  selfClosingTags = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr']
+
   def getTree(self):
     self.prune(self.tree)
     return self.tree
 
   def getLastChild(self, tree, depth):
     for i in range(depth):
-      tree = tree[-1]['children']
+      # Sometimes this tree searcher goes out of range but it's all good
+      try:
+        tree = tree[-1]['children']
+      except IndexError:
+        pass
     return tree
 
   # This method is run when the parser encounters an opening tag
   def handle_starttag(self, tag, attrs):
     attrlist = dict(attrs)
 
-    selfClosing = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr']
+    # Skip self-closing tags because it's the worst
+    if any(x in tag for x in self.selfClosingTags):
+      return
 
     # We have to go deeper
     self.depth += 1
 
-    # No class? Self-closing? Forget it
-    if not 'class' in attrlist or any(x in tag for x in selfClosing):
+    # No class? Forget it
+    if not 'class' in attrlist:
       return
     # Or skip this tag if it doesn't have any grid classes
     if not any(x in attrlist['class'].split(' ') for x in ['row', 'column', 'columns']):
@@ -50,6 +58,10 @@ class foundationParser(HTMLParser):
 
   # This method is run when the parser encounters a closing tag
   def handle_endtag(self, tag):
+    # Skip self-closing tags because it's the worst
+    if any(x in tag for x in self.selfClosingTags):
+      return
+
     # Move up one depth level
     self.depth -= 1
 
@@ -106,9 +118,6 @@ def encodeGrid(tree, root=True):
 
   output = ''
   for elem in tree:
-    # Debugging
-    # print "Element '%s' has '%s' children." % (elem['classes'], len(elem['children']))
-
     # If it's a column, get the classes
     if 'column' in elem['classes']:
       classes = elem['classes'].split(' ')
@@ -135,12 +144,187 @@ def encodeGrid(tree, root=True):
   return output
 
 syntax = """
-  <div class="row">
-    <div class="large-6 columns">
-    </div>
-    <div class="large-6 columns">
+   
+   <nav class="top-bar" data-topbar>
+  <ul class="title-area">
+     
+    <li class="name">
+      <h1>
+        <a href="#">
+          Top Bar Title
+        </a>
+      </h1>
+    </li>
+    <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
+  </ul>
+
+  <section class="top-bar-section">
+     
+    <ul class="right">
+      <li class="divider"></li>
+      <li class="has-dropdown">
+        <a href="#">Main Item 1</a>
+        <ul class="dropdown">
+          <li><label>Section Name</label></li>
+          <li class="has-dropdown">
+            <a href="#" class="">Has Dropdown, Level 1</a>
+            <ul class="dropdown">
+              <li><a href="#">Dropdown Options</a></li>
+              <li><a href="#">Dropdown Options</a></li>
+              <li><a href="#">Level 2</a></li>
+              <li><a href="#">Subdropdown Option</a></li>
+              <li><a href="#">Subdropdown Option</a></li>
+              <li><a href="#">Subdropdown Option</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Dropdown Option</a></li>
+          <li><a href="#">Dropdown Option</a></li>
+          <li class="divider"></li>
+          <li><label>Section Name</label></li>
+          <li><a href="#">Dropdown Option</a></li>
+          <li><a href="#">Dropdown Option</a></li>
+          <li><a href="#">Dropdown Option</a></li>
+          <li class="divider"></li>
+          <li><a href="#">See all →</a></li>
+        </ul>
+      </li>
+      <li class="divider"></li>
+      <li><a href="#">Main Item 2</a></li>
+      <li class="divider"></li>
+      <li class="has-dropdown">
+        <a href="#">Main Item 3</a>
+        <ul class="dropdown">
+          <li><a href="#">Dropdown Option</a></li>
+          <li><a href="#">Dropdown Option</a></li>
+          <li><a href="#">Dropdown Option</a></li>
+          <li class="divider"></li>
+          <li><a href="#">See all →</a></li>
+        </ul>
+      </li>
+    </ul>
+  </section>
+</nav>
+
+ 
+
+
+ 
+
+<div class="row">
+
+   
+  <div class="large-9 columns">
+
+    <h3>Get in Touch!</h3>
+    <p>We'd love to hear from you. You can either reach out to us as a whole and one of our awesome team members will get back to you, or if you have a specific question reach out to one of our staff. We love getting email all day <em>all day</em>.</p>
+
+    <div class="section-container tabs" data-section>
+      <section class="section">
+        <h5 class="title"><a href="#panel1">Contact Our Company</a></h5>
+        <div class="content" data-slug="panel1">
+          <form>
+            <div class="row collapse">
+              <div class="large-2 columns">
+                <label class="inline">Your Name</label>
+              </div>
+              <div class="large-10 columns">
+                <input type="text" id="yourName" placeholder="Jane Smith">
+              </div>
+            </div>
+            <div class="row collapse">
+              <div class="large-2 columns">
+                <label class="inline"> Your Email</label>
+              </div>
+              <div class="large-10 columns">
+                <input type="text" id="yourEmail" placeholder="jane@smithco.com">
+              </div>
+            </div>
+            <label>What's up?</label>
+            <textarea rows="4"></textarea>
+            <button type="submit" class="radius button">Submit</button>
+          </form>
+        </div>
+      </section>
+      <section class="section">
+        <h5 class="title"><a href="#panel2">Specific Person</a></h5>
+        <div class="content" data-slug="panel2">
+          <ul class="large-block-grid-5">
+            <li><a href="mailto:mal@serenity.bc.reb"><img src="http://placehold.it/200x200&text=[person]">Mal Reynolds</a></li>
+            <li><a href="mailto:zoe@serenity.bc.reb"><img src="http://placehold.it/200x200&text=[person]">Zoe Washburne</a></li>
+            <li><a href="mailto:jayne@serenity.bc.reb"><img src="http://placehold.it/200x200&text=[person]">Jayne Cobb</a></li>
+            <li><a href="mailto:doc@serenity.bc.reb"><img src="http://placehold.it/200x200&text=[person]">Simon Tam</a></li>
+            <li><a href="mailto:killyouwithmymind@serenity.bc.reb"><img src="http://placehold.it/200x200&text=[person]">River Tam</a></li>
+            <li><a href="mailto:leafonthewind@serenity.bc.reb"><img src="http://placehold.it/200x200&text=[person]">Hoban Washburne</a></li>
+            <li><a href="mailto:book@serenity.bc.reb"><img src="http://placehold.it/200x200&text=[person]">Shepherd Book</a></li>
+            <li><a href="mailto:klee@serenity.bc.reb"><img src="http://placehold.it/200x200&text=[person]">Kaywinnet Lee Fry</a></li>
+            <li><a href="mailto:inara@guild.comp.all"><img src="http://placehold.it/200x200&text=[person]">Inarra Serra</a></li>
+          </ul>
+        </div>
+      </section>
     </div>
   </div>
+
+   
+
+
+   
+
+
+  <div class="large-3 columns">
+    <h5>Map</h5>
+     
+    <p>
+      <a href="" data-reveal-id="mapModal"><img src="http://placehold.it/400x280"></a><br/>
+      <a href="" data-reveal-id="mapModal">View Map</a>
+    </p>
+    <p>
+      123 Awesome St.<br/>
+      Barsoom, MA 95155
+    </p>
+  </div>
+   
+</div>
+
+ 
+
+
+ 
+
+<footer class="row">
+  <div class="large-12 columns">
+    <hr/>
+    <div class="row">
+      <div class="large-6 columns">
+        <p>© Copyright no one at all. Go to town.</p>
+      </div>
+      <div class="large-6 columns">
+        <ul class="inline-list right">
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+          <li><a href="#">Link 3</a></li>
+          <li><a href="#">Link 4</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+
+ 
+
+
+
+ 
+
+<div class="reveal-modal" id="mapModal">
+  <h4>Where We Are</h4>
+  <p><img src="http://placehold.it/800x600"/></p>
+
+   
+  <a href="#" class="close-reveal-modal">×</a>
+</div>
+  
+  
+   
 """
 
 parser = foundationParser()
